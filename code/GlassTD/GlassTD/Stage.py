064ddad63925011cfc145d096642630cbbf7dbf7
kid <963262214@qq.com>
2012-05-29 19:01:03 +0800

    Stage.py StageManager.py
    Signed-off-by: kid <963262214@qq.com>

--- code/GlassTD/GlassTD/Stage.py
+++ code/GlassTD/GlassTD/Stage.py
@@ -4,7 +4,7 @@ Created on 2012-5-29
 @author: Kid
 '''
 
-from StageJumpEnum import StageJumpEnum
+from StageJumpEnum import *
 
 class Stage:
     """A game stage define the scene content, resources and logics.
@@ -14,21 +14,33 @@ class Stage:
         self.mSceneManager = sceneManager
         self.mIsInitialized = False
     
-    def initialize(self):    
+    def initialize(self):
+        """You should create you scene in this function"""
         pass
        
     def release(self):
+        """Called when jump to another stage.
+        Release all resources.
+        """
         pass
        
     def run(self):
+        """Run the stage.
+        @return: True means continue to run this stage.
+                 False means need to change another stage.
+        """
         pass
        
     def onEvent(self):
         pass
        
     def getNextStage(self):
+        """Get the stage id of next stage.
+        @return: The id of next stage.
+        """
         pass
     
     def isInitialized(self):
+        """Return if the stage is ready to run."""
         return self.mIsInitialized
     
--- code/GlassTD/GlassTD/StageManager.py
+++ code/GlassTD/GlassTD/StageManager.py
@@ -4,6 +4,8 @@ Created on 2012-5-29
 @author: Kid
 '''
 
+from Stage import *
+
 class StageManager:
     """Provide management on stages, include initialize, 
     release, run and jump.
@@ -23,16 +25,26 @@ class StageManager:
         self.mStages[stageId] = stage
          
     def setStartStage(self, stageId):
-        """Define which stage will be the entry."""
+        """Define which stage will be the entry.
+        @param stageId: the enum id that represent the stage"""
         self.mCurrentStage = self.mStages[stageId]
         
     def runStages(self):
         """Run the stages"""
+        # verify start stage
         if (self.mCurrentStage == None):
             raise Exception, \
                 "None current stage, you must first call setStartStage()"
+        # ini
         if (not self.mCurrentStage.isInitialized()):
             self.mCurrentStage.initialize()
+        # run
+        if (not self.mCurrentStage.run()):
+            # jump and release
+            nextStage = self.mCurrentStage.getNextStage()
+            self.mCurrentStage.release()
+            self.mCurrentStage = self.mStages[nextStage]
+            
         
         
         
