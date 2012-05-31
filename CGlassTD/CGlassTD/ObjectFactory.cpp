9b5f2d04a9bb24be85c4abe03870ec5cfeabccbd
zhendong <271243631@qq.com>
2012-05-31 11:04:43 +0800

    chang ObjectFactory.cpp

--- CGlassTD/CGlassTD/ObjectFactory.cpp
+++ CGlassTD/CGlassTD/ObjectFactory.cpp
@@ -10,7 +10,42 @@ ObjectFactory::~ObjectFactory(void)
 {
 }
 
-Entity ObjectFactory::getEntity( SceneManager* sceneManager,String mesh,String name )
+Ogre::Entity* ObjectFactory::getEntity( SceneManager* sceneManager,Ogre::String mesh,Ogre::String name )
 {
+	return sceneManager->createEntity(name,mesh);
 
 }
+
+Ogre::Entity* ObjectFactory::getEntity( Ogre::SceneManager* sceneManager,Ogre::String mesh )
+{
+	return sceneManager->createEntity(mesh);
+}
+
+Ogre::SceneNode* ObjectFactory::getSceneNode( Ogre::SceneManager* sceneManager,Ogre::Entity* entity )
+{
+	SceneNode* sceneNode = sceneManager->getRootSceneNode()->createChildSceneNode();
+	sceneNode->attachObject((MovableObject*)entity);
+	return sceneNode;
+}
+
+Ogre::SceneNode* ObjectFactory::getSceneNode( Ogre::SceneNode* parentNode,Ogre::Entity* entity )
+{
+	SceneNode* sceneNode = parentNode->createChildSceneNode();
+	sceneNode->attachObject((MovableObject*)entity);
+	return sceneNode;
+}
+
+Ogre::SceneNode* ObjectFactory::getSceneNode( Ogre::SceneManager* sceneManager,Ogre::Entity* entity,Ogre::String nodeName )
+{
+	SceneNode* sceneNode = sceneManager->getRootSceneNode()->createChildSceneNode(nodeName);
+	sceneNode->attachObject((MovableObject*)entity);
+	return sceneNode;
+}
+
+Ogre::SceneNode* ObjectFactory::getSceneNode( Ogre::SceneNode* parentNode,Ogre::Entity* entity,Ogre::String nodeName )
+{
+	SceneNode* sceneNode = parentNode->createChildSceneNode(nodeName);
+	sceneNode->attachObject((MovableObject*)entity);
+	return sceneNode;
+}
+
--- CGlassTD/CGlassTD/ObjectFactory.h
+++ CGlassTD/CGlassTD/ObjectFactory.h
@@ -1,8 +1,6 @@
 #include <OgreSceneManager.h>
 #include <OgreSceneNode.h>
-#include <string.h>
 using namespace Ogre;
-using namespace std;
 
 #ifndef __ObjectFactory_h_
 #define __ObjectFactory_h_
@@ -12,10 +10,12 @@ class ObjectFactory
 public:
 	ObjectFactory(void);
 	~ObjectFactory(void);
-	static Ogre::Entity getEntity(Ogre::SceneManager* sceneManager,String mesh,String name);
-	static Ogre::Entity getEntity(Ogre::SceneManager* sceneManager,String mesh);
-	static Ogre::SceneNode getSceneNode(Ogre::SceneManager* sceneManager,Ogre::Entity* entity);
-	static Ogre::SceneNode getSceneNode(Ogre::SceneNode* parentNode,Ogre::Entity* entity);
+	static Ogre::Entity* getEntity(Ogre::SceneManager* sceneManager,Ogre::String mesh,Ogre::String name);
+	static Ogre::Entity* getEntity(Ogre::SceneManager* sceneManager,Ogre::String mesh);
+	static Ogre::SceneNode* getSceneNode(Ogre::SceneManager* sceneManager,Ogre::Entity* entity,Ogre::String nodeName);
+	static Ogre::SceneNode* getSceneNode(Ogre::SceneNode* parentNode,Ogre::Entity* entity,Ogre::String nodeName);
+	static Ogre::SceneNode* getSceneNode(Ogre::SceneManager* sceneManager,Ogre::Entity* entity);
+	static Ogre::SceneNode* getSceneNode(Ogre::SceneNode* parentNode,Ogre::Entity* entity);
 };
 #endif
 
